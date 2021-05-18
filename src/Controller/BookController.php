0088aaea881b73764b4ef3020a9a759d4d2da22d
Antoine <antoine_avenia@hotmail.fr>
2021-05-18 14:51:42 +0200

    add namespace IsGranted

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -5,10 +5,11 @@ namespace App\Controller;
 use App\Entity\Book;
 use App\Form\BookType;
 use App\Repository\BookRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
  * @Route("/book")
