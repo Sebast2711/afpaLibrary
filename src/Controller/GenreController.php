1bd0dfd87baed4e4e81101603945e85771875c0d
Sebast2711 <oillosebast@gmail.com>
2021-05-18 13:15:39 +0200

    add type and genre controllers, forms, templates with make crud

---
+++ src/Controller/GenreController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Genre;
+use App\Form\Genre1Type;
+use App\Repository\GenreRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+/**
+ * @Route("/genre")
+ */
+class GenreController extends AbstractController
+{
+    /**
+     * @Route("/", name="genre_index", methods={"GET"})
+     */
+    public function index(GenreRepository $genreRepository): Response
+    {
+        return $this->render('genre/index.html.twig', [
+            'genres' => $genreRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="genre_show", methods={"GET"})
+     */
+    public function show(Genre $genre): Response
+    {
+        return $this->render('genre/show.html.twig', [
+            'genre' => $genre,
+        ]);
+    }
+
+
+    /**
+     * @Route("/new", name="genre_new", methods={"GET","POST"})
+     * @IsGranted("ROLE_ADMIN", statusCode=401, message="You do not have permission") 
+     */
+    public function new(Request $request): Response
+    {
+        $genre = new Genre();
+        $form = $this->createForm(Genre1Type::class, $genre);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($genre);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('genre_index');
+        }
+
+        return $this->render('genre/new.html.twig', [
+            'genre' => $genre,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="genre_edit", methods={"GET","POST"})
+     * @IsGranted("ROLE_ADMIN", statusCode=401, message="You do not have permission") 
+     */
+    public function edit(Request $request, Genre $genre): Response
+    {
+        $form = $this->createForm(Genre1Type::class, $genre);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('genre_index');
+        }
+
+        return $this->render('genre/edit.html.twig', [
+            'genre' => $genre,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/delete", name="genre_delete", methods={"POST"})
+     * @IsGranted("ROLE_ADMIN", statusCode=401, message="You do not have permission") 
+
+     */
+    public function delete(Request $request, Genre $genre): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$genre->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($genre);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('genre_index');
+    }
+}
---
+++ src/Controller/TypeController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Type;
+use App\Form\Type1Type;
+use App\Repository\TypeRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+/**
+ * @Route("/type")
+ */
+class TypeController extends AbstractController
+{
+    /**
+     * @Route("/", name="type_index", methods={"GET"})
+     */
+    public function index(TypeRepository $typeRepository): Response
+    {
+        return $this->render('type/index.html.twig', [
+            'types' => $typeRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="type_new", methods={"GET","POST"})
+     * @IsGranted("ROLE_ADMIN", statusCode=401, message="You do not have permission") 
+     */
+    public function new(Request $request): Response
+    {
+        $type = new Type();
+        $form = $this->createForm(Type1Type::class, $type);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($type);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('type_index');
+        }
+
+        return $this->render('type/new.html.twig', [
+            'type' => $type,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="type_show", methods={"GET"})
+     */
+    public function show(Type $type): Response
+    {
+        return $this->render('type/show.html.twig', [
+            'type' => $type,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="type_edit", methods={"GET","POST"})
+     * @IsGranted("ROLE_ADMIN", statusCode=401, message="You do not have permission") 
+     */
+    public function edit(Request $request, Type $type): Response
+    {
+        $form = $this->createForm(Type1Type::class, $type);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('type_index');
+        }
+
+        return $this->render('type/edit.html.twig', [
+            'type' => $type,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="type_delete", methods={"POST"})
+     * @IsGranted("ROLE_ADMIN", statusCode=401, message="You do not have permission") 
+     */
+    public function delete(Request $request, Type $type): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$type->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($type);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('type_index');
+    }
+}
