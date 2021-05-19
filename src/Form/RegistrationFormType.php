62d4f3dcb5f6322ab7e1af786a0ba4026e7d475c
Antoine <antoine_avenia@hotmail.fr>
2021-05-19 14:47:58 +0200

    add placeholder for password and confirm password

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -31,8 +31,8 @@ class RegistrationFormType extends AbstractType
             ->add('plainPassword', RepeatedType::class, [
                 'type' => PasswordType::class,
                 'invalid_message' => "Password confirmation is different",
-                'first_options' => ['label' => "Password"],
-                'second_options' => ['label' => "Confirm your password"],
+                'first_options' => ['label' => "Password", 'attr' => array('placeholder' => 'Enter password')],
+                'second_options' => ['label' => "Confirm your password", 'attr' => array('placeholder' => 'Confirm password')],
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
