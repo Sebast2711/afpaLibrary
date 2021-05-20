9ea191eecdac64a4c53c37a30229ade9675cad2c
Antoine <antoine_avenia@hotmail.fr>
2021-05-20 09:35:31 +0200

    add isgranted class

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -8,6 +8,7 @@ use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
